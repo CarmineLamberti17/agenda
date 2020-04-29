a17dd96826837b40dec93676e8fee69b138a2855
CarmineLamberti17 <62182828+CarmineLamberti17@users.noreply.github.com>
2020-04-29 18:58:51 +0200

    Update Contatto.java

--- Contatto.java
+++ Contatto.java
@@ -1,11 +1,11 @@
 class Contatto{
     public String nome;
     public String cognome;
-    public String numeroTel;
+    public String nTel;
     public String email;
     public Contatto next;
     
-    public Contatto(String nome, String cognome, String numeroTel, String email){
+    public Contatto(String nome, String cognome, String nTel, String email){
         this.nome=nome;
         this.cognome=cognome;
         this.numeroTel=numeroTel;
